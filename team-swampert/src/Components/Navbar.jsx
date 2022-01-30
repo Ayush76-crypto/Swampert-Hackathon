d41030f9155966c1f178947992df108e8f5e96e2
Shelly Suthar <84381373+Shelly011s@users.noreply.github.com>
2022-01-30 22:29:44 +0530

    Update Navbar.jsx

--- team-swampert/src/Components/Navbar.jsx
+++ team-swampert/src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./userContext/context";
+import {  signOut } from "firebase/auth";
+import { auth } from "../fbcongif";
 import logo from './assets/photos/logo.png'
 
 function Navbar() {
   return (
+  const { user } = useContext(UserContext);
+  const logout = async () => {
+    await signOut(auth);
+  };
+  const links = user?.email ? <a href="/" onClick={logout} >Logout</a> : <a href="/Login" >Login</a> ;
     <nav className="flex bg-black text-white h-20 items-center px-20 cursor-pointer">
       <img src={logo} alt="LOGO" style={{width:"100px",height:"100px"}}/>
       <ul className="cursor-pointer flex justify-end px-12 space-x-11 text-xl font-semibold ml-auto">
@@ -13,7 +21,7 @@ function Navbar() {
         <li><a href="/Blog">Blog</a></li>
       </ul>
       <button className="transition ease-in-out bg-custom-blue hover:bg-[#F38181] text-black duration-200 font-bold py-1 px-10 rounded-full hover:text-white h-12 text-xl">
-      <a href="/Login" >Login</a> 
+      {links}
       </button>
     </nav>
   );
