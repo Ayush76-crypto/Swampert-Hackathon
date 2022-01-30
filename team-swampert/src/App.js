5a3e818af945ad13a3a7e198fb4d9c095e1969c0
Ayush Verma <78358911+Ayush76-crypto@users.noreply.github.com>
2022-01-30 12:05:00 +0530

    aboutus page and font-family

--- team-swampert/src/App.js
+++ team-swampert/src/App.js
@@ -3,6 +3,7 @@ import Homepage from "./Components/Homepage/Homepage";
 import { NewsContextProvider } from "./Components/NewsPage/NewsContext";
 import News from "./Components/NewsPage/News";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutUs from "./Components/AboutUs";
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          <Route exact path="/About" element={<Homepage />} />
+          <Route exact path="/About" element={<AboutUs />} />
           <Route exact path="/News" element={<NewsContextProvider><News /> </NewsContextProvider>} />
           <Route exact path="/Marketplace" element={<Homepage />} />
           <Route exact path="/Blog" element={<Homepage />} />
